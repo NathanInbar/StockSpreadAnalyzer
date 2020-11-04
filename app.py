70d23938734022a4b454cb9a5db9f81e668632d4
Nathan Inbar <nathaninbar@gmail.com>
2020-11-03 20:02:42 -0800

    🛐 need to switch back to alpha_vantage

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 from flask_session import Session
-from alpha_vantage.timeseries import TimeSeries
+from api_key import API_KEY
+from datetime import datetime
+from iexfinance.stocks import get_historical_data 
 import json
 
 #relavant docs: https://www.alphavantage.co/documentation/#intraday
-
-#alpha vantage api key
-API_KEY="O3QZG39MGA8IMI1R"
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 Session(app)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -19,6 +19,37 @@ def index():
         return render_template("index.html")
     if request.method == "POST":
         symbol = request.form.get('symbol')
+        startdate = request.form.get('startdate').split('-')
+        enddate = request.form.get('dt').split('-')
+        swingduration = request.form.get('swingduration')
+
+        for i in range(0,len(startdate)):
+            startdate[i] = int(startdate[i])
+
+        for i in range(0,len(enddate)):
+            enddate[i] = int(enddate[i])
+
+        strt = datetime(startdate[0],startdate[1],startdate[2])
+        end = datetime(enddate[0],enddate[1],enddate[2])
+
+        strt = datetime(2019,1,1)
+        end = datetime(2019,1,2)
+
+        data = {}
+
+        try:
+            #data = get_historical_data(symbol,strt, end, token=API_KEY)
+            data = get_historical_data("TSLA",strt,end, token=API_KEY)
+        except Exception as e:
+            print(f'error:{e}')
+
+        print('debug')
+        for key in data:
+            print(data[key]['high'])
+
+
+        #print(f'S: {symbol}\nSD: {startdate}\nED: {enddate}\nSW: {swingduration}\n')
         return render_template("index.html", result=symbol)        
+
 
 app.run(debug=True)
